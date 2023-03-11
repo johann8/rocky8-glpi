e4c34928992cf2f3cf84fc2ebeb4926134553a3f
johann8 <jo.hahn@gmx.de>
2023-03-11 12:44:25 +0100

    add docker tag latest

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set variables
-_VERSION=10.0.6
+_VERSION=10.0.6.1
 
 # build image glpi
 docker build -t johann8/glpi:${_VERSION} .
@@ -12,12 +12,14 @@ if ! [ ${_BUILD} = 0 ]; then
 else
    echo "Docker Image build successful"
    docker images -a
+   docker tag johann8/glpi:${_VERSION} johann8/glpi:latest
 fi
 
 #push image to dockerhub
 if [ ${_BUILD} = 0 ]; then
    echo "Pushing docker images to dockerhub..."
    docker push johann8/glpi:${_VERSION}
+   docker push johann8/glpi:latest
    _PUSH=$?
    docker images -a |grep glpi
 fi
@@ -26,6 +28,7 @@ fi
 if [ ${_PUSH} = 0 ]; then
    echo "Deleting docker images..."
    docker rmi johann8/glpi:${_VERSION}
+   docker rmi johann8/glpi:latest
    docker rmi rockylinux:8
    docker rmi $(docker images -f "dangling=true" -q)
    docker images -a
